41d1535712e8a8024bcf2867c358f1a4fd9e52e3
Fedorov Semyon <fedorovedorov@gmail.com>
2023-10-15 13:42:33 +0300

    Изменен масштаб браузера в conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    browser.config.window_height = 800
-    browser.config.window_width = 600
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
     browser.config.timeout = 3
 
     yield
